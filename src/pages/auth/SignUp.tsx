76796924544239e69c7e716be6114737920b7898
amankothardeveloper <amankothari.developer@gmail.com>
2026-06-25 01:17:48 +0530

    Created Password Eye Toggle

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,68 +1,74 @@
 import Auth from "./Auth";
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
 
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-  
-} from "@/components/ui/field"
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { useState } from "react";
 
 const SignUp = () => {
+  const [isShowPassword, setIsShowPassword] = useState(false);
+  const showHidePassword = () => {
+    setIsShowPassword((prev) => !prev);
+  };
   return (
     <Auth>
-      
       <h2 className="text-3xl">Register</h2>
- <FieldGroup>
-      <Field>
-        <FieldLabel htmlFor="fieldgroup-name">Name</FieldLabel>
-        <Input id="fieldgroup-name" placeholder="Jordan Lee" />
-      </Field>
-      <Field>
-        <FieldLabel htmlFor="fieldgroup-email">Email</FieldLabel>
-        <Input
-          id="fieldgroup-email"
-          type="email"
-          placeholder="name@example.com"
-        />
-       
-       
-      </Field>
-       <Field>
-        <FieldLabel htmlFor="fieldgroup-password">Password</FieldLabel>
-        <Input
-          id="fieldgroup-password"
-          type="password"
-          placeholder=""
-        />
-       
-       
-      </Field>
-       <Field>
-        <FieldLabel htmlFor="fieldgroup-confirm-password">Confirm Password</FieldLabel>
-        <Input
-          id="fieldgroup-confirm-password"
-          type="password"
-          placeholder=""
-        />
-       
-       
-      </Field>
-    </FieldGroup>
-        <FieldGroup className="max-w-sm mt-5">
-      <Field orientation="horizontal">
-        <Checkbox id="terms-checkbox" name="terms-checkbox" />
-        <Label htmlFor="terms-checkbox">Accept terms and conditions</Label>
-      </Field>
-      
-    </FieldGroup>
-<Button variant="default" className=" p-4 w-full text-white mt-5">Submit</Button>
-{/* <Button variant="outline" className="bg-[#506EE4] p-4 w-full">  Button </Button> */}
-
+      <FieldGroup>
+        <Field>
+          <FieldLabel htmlFor="fieldgroup-name">Name</FieldLabel>
+          <Input id="fieldgroup-name" placeholder="Jordan Lee" />
+        </Field>
+        <Field>
+          <FieldLabel htmlFor="fieldgroup-email">Email</FieldLabel>
+          <Input
+            id="fieldgroup-email"
+            type="email"
+            placeholder="name@example.com"
+          />
+        </Field>
+        <Field>
+          <FieldLabel htmlFor="fieldgroup-password">Password</FieldLabel>
+          <InputGroup>
+            <InputGroupInput
+              id="fieldgroup-password"
+              type={isShowPassword ? "text" : "password"}
+              placeholder="Enter password"
+            />
+            <InputGroupAddon align="inline-end">
+              <div onClick={() => showHidePassword()} className="cursor-pointer" role="button">
+                {isShowPassword ? <EyeIcon /> : <EyeOffIcon />}
+              </div>
+            </InputGroupAddon>
+          </InputGroup>
+        </Field>
+        <Field>
+          <FieldLabel htmlFor="fieldgroup-confirm-password">
+            Confirm Password
+          </FieldLabel>
+          <Input
+            id="fieldgroup-confirm-password"
+            type="password"
+            placeholder=""
+          />
+        </Field>
+      </FieldGroup>
+      <FieldGroup className="max-w-sm mt-5">
+        <Field orientation="horizontal">
+          <Checkbox id="terms-checkbox" name="terms-checkbox" />
+          <Label htmlFor="terms-checkbox">Accept terms and conditions</Label>
+        </Field>
+      </FieldGroup>
+      <Button className="w-full mt-5" size="lg">
+        Submit
+      </Button>
     </Auth>
   );
 };
